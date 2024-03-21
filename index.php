727c59822894d2ef05e0c82dc3bf5dc2f0476ef2
roberto-palanga <r.palanga94@gmail.com>
2024-03-21 09:38:31 +0100

    fatto ieri dimenticato di committare

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dischi</title>
 
+    <!-- my styles -->
+    <link href="./style.css">
+
     <!-- bootstrap link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
@@ -22,19 +25,19 @@
     <div id="app">
 
 
-        <div>
+        <div >
             <nav class="navbar " style="background-color: #09131E;">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">
-                        <img src="/img/logo-spotify.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                        <img src="./img/logo-spotify.png" alt="Logo" width="40" height="40" class="d-inline-block align-text-top w-100 ">
 
                     </a>
                 </div>
             </nav>
-            <main class="container-fluid p-3 " style="background-color: #202C3B;">
-                <div class="container my-3  d-flex flex-wrap justify-content-center gap-3" style="margin-left= auto; margin-right= auto; ">
-                
-                    <div v-for="albums in Discs"  class="card " style="width: 18rem; background-color: #14202F; ">
+            <main class="container-fluid position-relative p-3  " style="background-color: #202C3B;">
+                <div class="container my-3  d-flex flex-wrap justify-content-center gap-3" style="margin-left: auto; margin-right: auto; ">
+
+                    <div v-for="(albums,index) in Discs" class="card " style="width: 18rem; background-color: #14202F; " @click="overLay(index)">
                         <img :src="albums.poster" class="card-img-top w-100" alt="...">
                         <div class="card-body text-center text-white">
                             <div class=" fw-bolder fs-4 ">
@@ -52,7 +55,33 @@
 
                         </div>
                     </div>
+
+
                     
+                    
+                    
+                </div>
+
+                <div v-if="overFoto"  class=" popup d-flex justify-content-center align-items-center  bg-transparent">
+                    <div class="card "  style="width: 18rem; background-color: #14202F;">
+                        <img :src="clickedFoto.poster"  class="card-img-top w-100" alt="...">
+                        <div class="card-body text-center text-white">
+                            <div class=" fw-bolder fs-4 ">
+                                {{clickedFoto.title}}
+
+                            </div>
+                            <div class=" fw-lighter">
+                                {{clickedFoto.year}}
+                            </div>
+                            <div class="fw-light fs-5">
+                                {{clickedFoto.author}}
+                            </div>
+
+
+
+                        </div>
+
+                    </div>
                 </div>
 
             </main>
--- js/main.js
+++ js/main.js
@@ -5,6 +5,19 @@ createApp({
     data() {
         return {
             Discs:[],
+            clickedFoto:[],
+            overFoto: false,
+        }
+    },
+
+    methods:{
+        overLay(currentIndex){
+            axios.get('./server.php').then(res => {
+                console.log(res.data[currentIndex]);
+
+                this.clickedFoto = res.data[currentIndex];
+                this.overFoto = true;
+            })
         }
     },
 
